11a2cf938c1c4f149c98b3cf799fd59bd7a1a612
BacsiIskola <iskolacc27@gmail.com>
2024-10-21 13:50:34 +0200

    color changes

---
+++ app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        entry.target.classList.add('show1');
+        entry.target.classList.add('sstack');
+      } else {
+        entry.target.classList.remove('show');
+        entry.target.classList.remove('show1');
+        entry.target.classList.remove('sstack');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden, .hidden1, .hstack');
+  hiddenElements.forEach((el) => observer.observe(el));
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
